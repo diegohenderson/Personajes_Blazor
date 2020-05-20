1949f85dd647b66e69a9895cdfa8e0ed14039b24
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2020-05-20 16:59:31 -0300

    Agregada algunas propiedades a la entidad personaje, y creada la api context (db context)

---
+++ Api_PracticaProfesionalizante.WebAPi/Data/ApiContext.cs
@@ -0,0 +1,18 @@
+﻿using Api_PracticaProfesionalizante.WebAPi.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_PracticaProfesionalizante.WebAPi.Data
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options): base(options)
+        {
+
+        }
+        public DbSet<Personaje> Personajes { get; set; }
+    }
+}
--- Api_PracticaProfesionalizante.WebAPi/Data/Entities/Personaje.cs
+++ Api_PracticaProfesionalizante.WebAPi/Data/Entities/Personaje.cs
@@ -7,5 +7,12 @@ namespace Api_PracticaProfesionalizante.WebAPi.Data.Entities
 {
     public class Personaje
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public string Localizacion { get; set; }
+        public string Origen { get; set; }
+
     }
 }

20f8b63a3b6a8f705d69b48e736a76be2e44efd5
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2020-05-20 22:36:29 -0300

    Modificado el api context, agregada la conexion en startup,   y agregada la "string" de conexion en nuestro appsetting.json

---
+++ Api_PracticaProfesionalizante.WebAPi/Migrations/20200521013352_Inicio.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api_PracticaProfesionalizante.WebAPi.Migrations
+{
+    public partial class Inicio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personajes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(nullable: true),
+                    Apellido = table.Column<string>(nullable: true),
+                    Edad = table.Column<int>(nullable: false),
+                    Localizacion = table.Column<string>(nullable: true),
+                    Origen = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personajes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Personajes");
+        }
+    }
+}

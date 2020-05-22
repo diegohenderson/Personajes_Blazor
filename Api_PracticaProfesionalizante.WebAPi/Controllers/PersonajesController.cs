78bf298ebe24ac2b0aadc227a69a4b30f2c4ffd8
Diego Enrique Henderson <diegohenderson425@outlook.com>
2020-05-22 00:04:40 -0300

    .....

---
+++ Api_PracticaProfesionalizante.WebAPi/Controllers/PersonajesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api_PracticaProfesionalizante.WebAPi.Data;
+using Api_PracticaProfesionalizante.WebAPi.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_PracticaProfesionalizante.WebAPi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonajesController : ControllerBase
+    {
+        private readonly ApiContext context;
+
+        public PersonajesController(ApiContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Personaje>>Get()
+        {
+
+        }
+    }
+}
--- Api_PracticaProfesionalizante.WebAPi/Data/Entities/Personaje.cs
+++ Api_PracticaProfesionalizante.WebAPi/Data/Entities/Personaje.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace Api_PracticaProfesionalizante.WebAPi.Data.Entities
     public class Personaje
     {
         public int Id { get; set; }
+        
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public int Edad { get; set; }

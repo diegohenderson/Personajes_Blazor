7562d4d7685765ea0aaccc2180c7667b5e1f6738
Diego Enrique Henderson <diegohenderson425@outlook.com>
2020-05-23 20:28:00 -0300

    Agregado los metodos Get y Post (Asyncrono) en Personajes Controller

--- Api_PracticaProfesionalizante.WebAPi/Controllers/PersonajesController.cs
+++ Api_PracticaProfesionalizante.WebAPi/Controllers/PersonajesController.cs
@@ -18,10 +18,42 @@ namespace Api_PracticaProfesionalizante.WebAPi.Controllers
         {
             this.context = context;
         }
+        #region Get
+        //Get de La lista Entera.
         [HttpGet]
         public ActionResult<IEnumerable<Personaje>>Get()
         {
+            return context.Personajes.ToList();
+        }
+
+        //por id.
+        [HttpGet("{id}", Name = "ObtenerPersonajePorID")]
+        public ActionResult<Personaje> Get(int id)
+        {
+            var personaje = context.Personajes.FirstOrDefault(p => p.Id == id);
+            if (personaje == null)
+            {
+                return NotFound();
+
+            }
+            return personaje;
+        }
+
+        #endregion
+        #region Post
+
+        [HttpPost]     
+        public async Task<ActionResult<Personaje>> Post([FromBody] Personaje personaje)
+        {
+            
+            await context.Personajes.AddAsync(pais);
+            
+            await context.SaveChangesAsync();
+            return new CreatedAtRouteResult("obtenerPersonajePorId", new { id = personaje.Id }, personaje);
+            
 
         }
+        #endregion
+
     }
 }

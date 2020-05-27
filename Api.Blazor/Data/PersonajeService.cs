95ed0a6eec7b5365dca927fe6928097ebafd2645
Diego Enrique Henderson <diegohenderson425@outlook.com>
2020-05-27 14:37:14 -0300

    Ahora esta api de personajes, tiene su propiom proyecto blazor (client-side)

---
+++ Api.Blazor/Data/PersonajeService.cs
@@ -0,0 +1,23 @@
+﻿using Api.Blazor.Modelos;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Api.Blazor.Data
+{
+    public class PersonajeServices
+    {
+
+        public async Task<Personaje> GetTaskByIdAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+            var personaje = await httpClient.GetAsync("https://localhost:44383/api/personajes/3");
+            var response = personaje.Content.ReadAsStringAsync();
+            Personaje personaje1 = JsonConvert.DeserializeObject<Personaje>(response.Result);
+            return personaje1;
+        }
+    }
+}
---
+++ Api.Blazor/Modelos/Personaje.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Blazor.Modelos
+{
+    public class Personaje
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Localizacion { get; set; }
+        //todo agregar un array para amigos, en los 2 modelos
+        //
+
+    }
+}
